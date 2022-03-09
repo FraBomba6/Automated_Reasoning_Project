fb4a373d4cea23dc625d900db5de4f581230c005
FraBomba6 <bombasseidebona.francesco@spes.uniud.it>
2022-03-09 15:32:11 +0100

    Refactored plotting script to extract common utilities and modified the script itself

--- python/grid_plotter.py
+++ python/grid_plotter.py
@@ -4,71 +4,37 @@ import sys
 import chime
 import tqdm
 import numpy as np
-import math
 import matplotlib.pyplot as pl
-
-HEARTS_SYMBOL = u"\u2665"
-CLUBS_SYMBOL = u"\u2663"
-SPADES_SYMBOL = u"\u2660"
-DIAMONDS_SYMBOL = u"\u2666"
-BLOCKED_SYMBOL = u"\u2612"
+import utils
 
 MIN_TABLE_HEIGHT = 2.5
-
-
-def get_symbol(value: int):
-    if value == 0:
-        return ""
-    elif value == 1:
-        return HEARTS_SYMBOL
-    elif value == 2:
-        return CLUBS_SYMBOL
-    elif value == 3:
-        return SPADES_SYMBOL
-    elif value == 4:
-        return DIAMONDS_SYMBOL
-    else:
-        return BLOCKED_SYMBOL
-
-
-def symbolize_matrix(matrix: np.ndarray):
-    symbolized_matrix = np.empty(matrix.shape, dtype=str)
-    for (x, y), value in np.ndenumerate(matrix):
-        symbolized_matrix[x, y] = get_symbol(value)
-    return symbolized_matrix
-
-
-def build_steps_from_json(steps):
-    steps_list = []
-    for step in steps:
-        steps_list.append(Step(step["step"], step["m"], step["score"]))
-    return steps_list
-
-
-def build_ndarray(matrix: list):
-    n = int(math.sqrt(len(matrix)))
-    return np.array(matrix).reshape(n, n)
 
 
 def check_dir_existence(path: str):
     return os.path.isdir(path)
 
 
-def gen_matrix_fig(matrix, hearts, clubs, spades, diamonds, path, score=None, execution_time=None):
-    n = matrix.shape[0]
-    plotHeight = max(MIN_TABLE_HEIGHT, n * 0.5)
-    symbol_matrix = symbolize_matrix(matrix)
+def gen_matrix_fig(matrix, hearts, clubs, spades, diamonds, path, colors, score=None, execution_time=None):
+    matrix_size = matrix.shape[0]
+    plotHeight = max(MIN_TABLE_HEIGHT, matrix_size * 0.5)
+    symbol_matrix = utils.symbolize_matrix(matrix)
     fontSize = 7
     textSize = 0.0138 * fontSize
-    colWidths = [0.2] * n
-    tableHeight = 0.225 * n
-    horizontalPadding = (1.5 - 0.2 * n) / 2
+    colWidths = [0.2] * matrix_size
+    tableHeight = 0.225 * matrix_size
+    horizontalPadding = (1.5 - 0.2 * matrix_size) / 2
     pl.figure(figsize=(1.5, plotHeight), dpi=200)
     pl.table(
         cellText=symbol_matrix,
         colWidths=colWidths,
-        bbox=[horizontalPadding / 1.5, (plotHeight - tableHeight) / plotHeight, 0.2 * n / 1.5, tableHeight / plotHeight],
-        cellLoc='center'
+        bbox=[
+            horizontalPadding / 1.5,
+            (plotHeight - tableHeight) / plotHeight,
+            0.2 * matrix_size / 1.5,
+            tableHeight / plotHeight
+        ],
+        cellLoc='center',
+        cellColours=colors
     )
     ax = pl.gca()
     ax.set_xticks([])
@@ -77,50 +43,50 @@ def gen_matrix_fig(matrix, hearts, clubs, spades, diamonds, path, score=None, ex
     pl.text(
         0.05,
         0.5 - textSize * 2 / plotHeight - 0.015,
-        "#" + HEARTS_SYMBOL + "=" + str(hearts - np.count_nonzero(matrix == 1)),
+        "#" + utils.HEARTS_SYMBOL + "=" + str(hearts - np.count_nonzero(matrix == 1)),
         fontsize=fontSize
     )
     pl.text(
         0.05,
         0.5 - textSize * 3 / plotHeight - 0.045,
-        "#" + CLUBS_SYMBOL + "=" + str(clubs - np.count_nonzero(matrix == 2)),
+        "#" + utils.CLUBS_SYMBOL + "=" + str(clubs - np.count_nonzero(matrix == 2)),
         fontsize=fontSize
     )
     pl.text(
         0.05,
         0.5 - textSize * 4 / plotHeight - 0.075,
-        "#" + SPADES_SYMBOL + "=" + str(spades - np.count_nonzero(matrix == 3)),
+        "#" + utils.SPADES_SYMBOL + "=" + str(spades - np.count_nonzero(matrix == 3)),
         fontsize=fontSize
     )
     pl.text(
         0.05,
         0.5 - textSize * 5 / plotHeight - 0.105,
-        "#" + DIAMONDS_SYMBOL + "=" + str(diamonds - np.count_nonzero(matrix == 4)),
+        "#" + utils.DIAMONDS_SYMBOL + "=" + str(diamonds - np.count_nonzero(matrix == 4)),
         fontsize=fontSize
     )
     pl.text(0.55, 0.5 - textSize / plotHeight, "Total", fontsize=fontSize)
     pl.text(
         0.55,
         0.5 - textSize * 2 / plotHeight - 0.015,
-        "#" + HEARTS_SYMBOL + "=" + str(hearts),
+        "#" + utils.HEARTS_SYMBOL + "=" + str(hearts),
         fontsize=fontSize
     )
     pl.text(
         0.55,
         0.5 - textSize * 3 / plotHeight - 0.045,
-        "#" + CLUBS_SYMBOL + "=" + str(clubs),
+        "#" + utils.CLUBS_SYMBOL + "=" + str(clubs),
         fontsize=fontSize
     )
     pl.text(
         0.55,
         0.5 - textSize * 4 / plotHeight - 0.075,
-        "#" + SPADES_SYMBOL + "=" + str(spades),
+        "#" + utils.SPADES_SYMBOL + "=" + str(spades),
         fontsize=fontSize
     )
     pl.text(
         0.55,
         0.5 - textSize * 5 / plotHeight - 0.105,
-        "#" + DIAMONDS_SYMBOL + "=" + str(diamonds),
+        "#" + utils.DIAMONDS_SYMBOL + "=" + str(diamonds),
         fontsize=fontSize
     )
     if score is not None:
@@ -149,28 +115,66 @@ def gen_matrix_fig(matrix, hearts, clubs, spades, diamonds, path, score=None, ex
     pl.close()
 
 
-class Result:
-    def __init__(self, json_result):
-        self.n = json_result["n"]
-        self.hearts = json_result["hearts"]
-        self.clubs = json_result["clubs"]
-        self.spades = json_result["spades"]
-        self.diamonds = json_result["diamonds"]
-        self.start_matrix = build_ndarray(json_result["m"])
-        self.steps = build_steps_from_json(json_result["steps"])
-        self.exe_time = json_result["execution_time"]
+def gen_matrix_plot(mat_size: int, step_num: int, is_mzn: bool, result: utils.Result):
+    if is_mzn:
+        current_dir = "../minizinc/results/size_" + str(mat_size) + "/" + str(step_num)
+    else:
+        current_dir = "../asp/results/size_" + str(mat_size) + "/" + str(step_num)
 
+    if not check_dir_existence(current_dir):
+        os.makedirs(current_dir)
 
-class Step:
-    def __init__(self, number: int, matrix: list, score: int):
-        self.number = number
-        self.matrix = build_ndarray(matrix)
-        self.score = score
+    colors_matrix = []
+    for row in result.start_matrix:
+        row_colors = []
+        for col in row:
+            if col == 0:
+                row_colors.append("w")
+            elif col == 5:
+                row_colors.append("#ff5733")
+            else:
+                row_colors.append("#ffc300")
+        colors_matrix.append(row_colors)
+
+    gen_matrix_fig(
+        result.start_matrix,
+        result.hearts,
+        result.clubs,
+        result.spades,
+        result.diamonds,
+        current_dir + "/start.png",
+        colors_matrix
+    )
+
+    for count, step in enumerate(result.steps):
+        if count == len(result.steps) - 1:
+            gen_matrix_fig(
+                step.matrix,
+                result.hearts,
+                result.clubs,
+                result.spades,
+                result.diamonds,
+                current_dir + "/step_" + str(step.number) + ".png",
+                colors_matrix,
+                step.score,
+                result.exe_time
+            )
+        else:
+            gen_matrix_fig(
+                step.matrix,
+                result.hearts,
+                result.clubs,
+                result.spades,
+                result.diamonds,
+                current_dir + "/step_" + str(step.number) + ".png",
+                colors_matrix,
+                step.score
+            )
 
 
 if __name__ == '__main__':
     for arg in range(1, len(sys.argv)):
-        file = "../results/" + sys.argv[arg]
+        file = sys.argv[arg]
         with open(file, 'r') as f:
             results = json.load(f)
             f.close()
@@ -179,84 +183,12 @@ if __name__ == '__main__':
             print("Working on size " + str(n) + "...")
             matrix_pbar = tqdm.tqdm(range(1, 21))
             for i in matrix_pbar:
-                mzn_result = Result(results[key]["mzn"][str(i)])
-                asp_result = Result(results[key]["asp"][str(i)])
+                mzn_result = utils.Result(results[key]["mzn"][str(i)])
+                asp_result = utils.Result(results[key]["asp"][str(i)])
 
-                current_mzn_dir = "../minizinc/results/size_" + str(n) + "/" + str(i)
-                if not check_dir_existence(current_mzn_dir):
-                    os.makedirs(current_mzn_dir)
+                gen_matrix_plot(n, i, True, mzn_result)
+                gen_matrix_plot(n, i, False, asp_result)
 
-                gen_matrix_fig(
-                    mzn_result.start_matrix,
-                    mzn_result.hearts,
-                    mzn_result.clubs,
-                    mzn_result.spades,
-                    mzn_result.diamonds,
-                    current_mzn_dir + "/start.png"
-                )
-
-                step_pbar = tqdm.tqdm(mzn_result.steps)
-                for count, step in enumerate(step_pbar):
-                    if count == len(mzn_result.steps) - 1:
-                        gen_matrix_fig(
-                            step.matrix,
-                            mzn_result.hearts,
-                            mzn_result.clubs,
-                            mzn_result.spades,
-                            mzn_result.diamonds,
-                            current_mzn_dir + "/step_" + str(step.number) + ".png",
-                            step.score,
-                            mzn_result.exe_time
-                        )
-                    else:
-                        gen_matrix_fig(
-                            step.matrix,
-                            mzn_result.hearts,
-                            mzn_result.clubs,
-                            mzn_result.spades,
-                            mzn_result.diamonds,
-                            current_mzn_dir + "/step_" + str(step.number) + ".png",
-                            step.score
-                        )
-                    step_pbar.set_description("Plotting Minizinc step # %d" % count)
-
-                current_asp_dir = "../asp/results/size_" + str(n) + "/" + str(i)
-                if not check_dir_existence(current_asp_dir):
-                    os.makedirs(current_asp_dir)
-
-                gen_matrix_fig(
-                    asp_result.start_matrix,
-                    asp_result.hearts,
-                    asp_result.clubs,
-                    asp_result.spades,
-                    asp_result.diamonds,
-                    current_asp_dir + "/start.png"
-                )
-
-                step_pbar = tqdm.tqdm(asp_result.steps)
-                for count, step in enumerate(step_pbar):
-                    if count == len(asp_result.steps) - 1:
-                        gen_matrix_fig(
-                            step.matrix,
-                            asp_result.hearts,
-                            asp_result.clubs,
-                            asp_result.spades,
-                            asp_result.diamonds,
-                            current_asp_dir + "/step_" + str(step.number) + ".png",
-                            step.score,
-                            asp_result.exe_time
-                        )
-                    else:
-                        gen_matrix_fig(
-                            step.matrix,
-                            asp_result.hearts,
-                            asp_result.clubs,
-                            asp_result.spades,
-                            asp_result.diamonds,
-                            current_asp_dir + "/step_" + str(step.number) + ".png",
-                            step.score
-                        )
-                    step_pbar.set_description("Plotting ASP step # %d" % count)
                 matrix_pbar.set_description("Processing matrix # %d" % i)
 
     chime.theme("big-sur")
---
+++ python/utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+import math
+
+HEARTS_SYMBOL = u"\u2661"
+CLUBS_SYMBOL = u"\u2663"
+SPADES_SYMBOL = u"\u2660"
+DIAMONDS_SYMBOL = u"\u2662"
+BLOCKED_SYMBOL = u"\u2610"
+
+
+def get_symbol(value: int):
+    if value == 0:
+        return ""
+    elif value == 1:
+        return HEARTS_SYMBOL
+    elif value == 2:
+        return CLUBS_SYMBOL
+    elif value == 3:
+        return SPADES_SYMBOL
+    elif value == 4:
+        return DIAMONDS_SYMBOL
+    else:
+        return BLOCKED_SYMBOL
+
+
+def symbolize_matrix(matrix: np.ndarray):
+    symbolized_matrix = np.empty(matrix.shape, dtype=str)
+    for (x, y), value in np.ndenumerate(matrix):
+        symbolized_matrix[x, y] = get_symbol(value)
+    return symbolized_matrix
+
+
+def build_steps_from_json(steps):
+    steps_list = []
+    for step in steps:
+        steps_list.append(Step(step["step"], step["m"], step["score"]))
+    return steps_list
+
+
+def build_ndarray(matrix: list):
+    n = int(math.sqrt(len(matrix)))
+    return np.array(matrix).reshape(n, n)
+
+
+class Result:
+    def __init__(self, json_result):
+        self.n = json_result["n"]
+        self.hearts = json_result["hearts"]
+        self.clubs = json_result["clubs"]
+        self.spades = json_result["spades"]
+        self.diamonds = json_result["diamonds"]
+        self.start_matrix = build_ndarray(json_result["m"])
+        self.steps = build_steps_from_json(json_result["steps"])
+        self.exe_time = json_result["execution_time"]
+
+
+class Step:
+    def __init__(self, number: int, matrix: list, score: int):
+        self.number = number
+        self.matrix = build_ndarray(matrix)
+        self.score = score
